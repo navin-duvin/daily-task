2fb1f2cf691b0a0df9e13fc373bc1e181fc57072
Navinduvin <71208443+navin-duvin@users.noreply.github.com>
2022-04-25 12:59:00 +0530

    Update dollarpat.cpp

--- C++/day 2/dollarpat.cpp
+++ C++/day 2/dollarpat.cpp
@@ -18,12 +18,12 @@ int main(){
 	int n=5;
 	for(int i = 1;i <= n;i++){
 		for(int j = 0;j < n - i;j++){
-			printf(" ");
+			cout<<" ";
 		}
 		for(int k = 0;k < i;k++){
 			cout<<"$ ";
 		}
-		printf("\n");
+		cout<<"\n";
 	}
 	return 0;
 }
